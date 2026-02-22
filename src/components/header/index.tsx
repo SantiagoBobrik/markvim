18d7b6c4d14b6b700eded134fe88d64ed0516280
Santiago Bobrik <santiago.bobrik@gmail.com>
2026-02-22 02:35:49 -0300

    feat: add GitHub repository link to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,4 @@
+import { GitHub } from "../../assets/GitHub";
 import { DropDown } from "../dropdown";
 import { Logo } from "./Logo";
 import "./index.css";
@@ -6,7 +7,18 @@ export const Header = () => {
   return (
     <header className="main-header">
       <Logo />
-      <DropDown />
+      <div className="header-actions">
+        <a
+          href="https://github.com/SantiagoBobrik/markvim"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github-link"
+          aria-label="GitHub repository"
+        >
+          <GitHub width={20} height={20} />
+        </a>
+        <DropDown />
+      </div>
     </header>
   );
 };
